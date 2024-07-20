05aabdb560cc14b4a99d4043cdac126d843d1ded
Jooma <jamie.gworld@hotmail.com>
2024-07-20 23:12:08 +0200

    created LinearRegression class

---
+++ ft_linear_regression.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+class LinearRegression():
+    """
+    Description:
+    My personnal linear regression class and its methods.
+    """
